9185f0e4625173dfb9e4e32ac0563733b2abef6e
nxroot1403 Nam <namnx@gmail.com>
2020-04-21 18:41:48 +0700

    perf: performance for model: category, tag

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
-  has_many :posts_relationship, as: :posts_relationship
+  has_many :posts, as: :posts_relationship
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Tag < ApplicationRecord
-  has_many :posts_relationship, as: :posts_relationship
+  has_many :posts, as: :posts_relationship
 end
