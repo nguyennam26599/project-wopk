d2c63e9715e11efdd3c7a255ceea375dde111f4d
Huy Hung <hungbh@gmail.com>
2020-04-23 17:41:25 +0700

    refactor: fixing syntax rspec

--- spec/views/pages/index.html.erb_spec.rb
+++ spec/views/pages/index.html.erb_spec.rb
@@ -3,5 +3,5 @@
 require 'rails_helper'
 
 RSpec.describe 'pages/index.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
 end
