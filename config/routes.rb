99c83ccb9d8f35d99a092ca7c69de504f9bf63cd
hoang <hoang@hoangs-MacBook-Pro.local>
2020-04-23 20:19:44 +0700

    fix: name css

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
   devise_scope :user do
     get 'login', to: 'users/sessions#new'
@@ -15,7 +14,4 @@ Rails.application.routes.draw do
     get 'table' => 'pages#table'
   end
   root 'home#index'
-=======
-  root to: 'home#index'
->>>>>>> feat: create mock index
 end
