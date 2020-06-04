e389dfb275fedf9a1959c1ed3f8b86ed0147d0df
ViHoang <vihoang@MacBook-Pro-cua-ViHoang.local>
2020-06-04 10:59:43 +0700

    feat: add data page idex

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module UsersHelper
+  include Pagy::Frontend
   def avatar_for(user, avt_class, id_avatar)
     if user.avatar.blank?
       image_tag('/default-user-avatar.png', class: avt_class.to_s, alt: user.name, id: id_avatar.to_s)
