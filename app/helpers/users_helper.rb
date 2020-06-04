deb9221d21c0ff10621a5cac3c111781d5f76229
Huy Hung <huyo.hung98@gmail.com>
2020-06-04 15:11:22 +0700

    add validate input avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,10 @@ module UsersHelper
       image_tag(user.avatar, class: avt_class.to_s, alt: user.name, id: id_avatar.to_s)
     end
   end
+
+  def regex_name_validate
+    "^[a-zA-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶ" +
+    "ẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợ" +
+    "ụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\\s]+$"
+  end
 end
