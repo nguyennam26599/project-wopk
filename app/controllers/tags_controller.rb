da3054b92bce629ec073222cce4fd770b8f5c5e6
namnx <namnx@gmail.com>
2020-06-04 08:52:38 +0700

    refactor: change code tag controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,6 +3,6 @@
 class TagsController < ApplicationController
   def search
     @tags = Tag.publish.search_name(params[:term]).limit(5)
-    render json: @tags.map(&:name)
+    render json: @tags.pluck(:name)
   end
 end
