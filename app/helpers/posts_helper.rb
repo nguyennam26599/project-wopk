93c65e8c9114ad11e0dc7512516e586beacfcbb2
Huy Hung <huyo.hung98@gmail.com>
2020-06-05 09:12:14 +0700

    hot fix: develop

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 module PostsHelper
-  def avatar_for(user, avt_class)
-    if @avatar.blank?
-      image_tag('/default-user-avatar.png', class: avt_class.to_s, alt: user.name)
-    else
-      image_tag(user.avatar, class: avt_class.to_s, alt: user.name)
-    end
-  end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -5,8 +5,9 @@ require 'rails_helper'
 RSpec.describe Tag, type: :model do
   it 'returns tag that match the search name and status' do
     tag2 = Tag.create(
-      name: 'This is the second tag.'
+      name: 'This is the second tag.',
+      status: 'publish'
     )
-    expect(Tag.search_name('second')).to include(tag2)
+    expect(Tag.search_name_status_public('This')).to include(tag2)
   end
 end
