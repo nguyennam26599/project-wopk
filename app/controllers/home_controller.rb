193ea5b3f19d55e168a4a93e0c8a9aa1ff3f1a1e
ViHoang <vihoang@MacBook-Pro-cua-ViHoang.local>
2020-06-09 11:09:52 +0700

    fix: conflix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,15 +7,15 @@ class HomeController < ApplicationController
   end
 
   def index
-    @pagy_daily, @posts_daily = pagy(Post.all.where(status: "pending").order(created_at: :desc).order(view_count: :desc), page_param: :page)
+    @pagy_daily, @posts_daily = pagy(Post.all.where(status: 'pending').order(created_at: :desc).order(view_count: :desc))
   end
 
   def posts_weekly
-    @pagy_weekly, @posts_weekly = pagy(Post.all.where(status: "pending").this_week.order(view_count: :desc), page_param: :page)
+    @pagy_weekly, @posts_weekly = pagy(Post.all.where(status: 'pending').this_week.order(view_count: :desc))
   end
 
   def posts_monthly
-    @pagy_monthly, @posts_monthly = pagy(Post.all.where(status: "pending").this_month.order(view_count: :desc), page_param: :page)
+    @pagy_monthly, @posts_monthly = pagy(Post.all.where(status: 'pending').this_month.order(view_count: :desc))
   end
 
   def tagfeed
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@
 module ApplicationHelper
   include Pagy::Frontend
   NUMBER_PAGE = 15
+
+  def show_header
+    exception_path = [login_path, signup_path, confirm_path, new_user_session_path, new_admin_session_path]
+    render partial: 'home/header' unless exception_path.include? request.path
+  end
+
+  def show_footer
+    exception_path = [login_path, signup_path, confirm_path, new_user_session_path, new_admin_session_path]
+    render partial: 'home/footer' unless exception_path.include? request.path
+  end
 end
