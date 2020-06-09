742f927f88514cb94f6cfc3ff5c712f94f2ed765
ViHoang <vihoang@MacBook-Pro-cua-ViHoang.local>
2020-06-09 09:23:16 +0700

    feat: only show post pending

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,15 +7,15 @@ class HomeController < ApplicationController
   end
 
   def index
-    @pagy_daily, @posts_daily = pagy(Post.all.order(created_at: :desc).order(view_count: :desc), page_param: :page)
+    @pagy_daily, @posts_daily = pagy(Post.all.where(status: "pending").order(created_at: :desc).order(view_count: :desc), page_param: :page)
   end
 
   def posts_weekly
-    @pagy_weekly, @posts_weekly = pagy(Post.all.this_week.order(view_count: :desc), page_param: :page)
+    @pagy_weekly, @posts_weekly = pagy(Post.all.where(status: "pending").this_week.order(view_count: :desc), page_param: :page)
   end
 
   def posts_monthly
-    @pagy_monthly, @posts_monthly = pagy(Post.all.this_month.order(view_count: :desc), page_param: :page)
+    @pagy_monthly, @posts_monthly = pagy(Post.all.where(status: "pending").this_month.order(view_count: :desc), page_param: :page)
   end
 
   def tagfeed
