3b9545d9caea43421595a93bc6d08d43f66ee360
Huy Hung <huyo.hung98@gmail.com>
2020-06-11 15:24:05 +0700

    fixing syntax

--- app/controllers/admins/posts_controller.rb
+++ app/controllers/admins/posts_controller.rb
@@ -2,17 +2,22 @@
 
 module Admins
   class PostsController < BaseController
+    before_action :set_post, only: %i[show]
+
     def index
       scope_post = params[:scope] || Post::PENDING_STATUS
       @pagy, @posts = pagy(Post.where(status: scope_post))
     end
-    
+
     def show
-      if (@post = Post.find_by(id: params[:id]))
-        @user = @post.user
-      else
-        redirect_to(admins_root_path)
-      end
+      @user = @post.user
+    end
+
+    private
+
+    def set_post
+      @post = Post.find_by(id: params[:id])
+      return redirect_to admins_root_path unless @post.present?
     end
   end
 end
