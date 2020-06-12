6fd50e4c0ed7e21c7ecb613e8f73623bdabbb8e1
ViHoang <vihoang@MacBook-Pro-cua-ViHoang.local>
2020-06-12 15:02:24 +0700

    fix: change pending to public

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -21,4 +21,50 @@ RSpec.describe Post, type: :model do
   it 'return a view count' do
     expect(subject.view_count == 1000).to be true
   end
+
+  describe 'scopes' do
+    before do
+      @post2 = Post.create(
+        title: 'test1',
+        user_id: 1
+      )
+      @user1 = User.create(
+        name: 'hoangvv',
+        email: 'test1234@gmail.com',
+        password: '123456'
+      )
+    end
+    it 'orders by ascending create_at' do
+      @post1 = Post.create(
+        title: 'test2',
+        user_id: 1
+      )
+      Post.sort_new_post == [@post1, @post2]
+    end
+    it 'orders by ascending count view' do
+      @post1 = Post.create(
+        title: 'test2',
+        user_id: 1,
+        view_count: '3123'
+      )
+      Post.sort_trend_post == [@post1, @post2]
+    end
+    it 'orders by ascending count view monthly' do
+      @post1 = Post.create(
+        title: 'test2',
+        user_id: 1,
+        view_count: '3123'
+      )
+      Post.top_post_public_this_month == [@post1, @post2]
+    end
+    it 'not post monthly' do
+      @post1 = Post.create(
+        title: 'test2',
+        user_id: 1,
+        view_count: '3123',
+        created_at: '2020-05-10 08:50:38'
+      )
+      Post.top_post_public_this_month == [@post2]
+    end
+  end
 end
