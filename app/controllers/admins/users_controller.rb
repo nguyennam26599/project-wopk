c6e4a5d158a4389ccc2d3e1165cc68063ad34083
Huy Hung <huyo.hung98@gmail.com>
2020-06-12 14:15:28 +0700

    feat: user detail in admin

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -2,8 +2,20 @@
 
 module Admins
   class UsersController < BaseController
+    before_action :set_user, only: %i[show]
+
     def index
       @pagy, @users = pagy(User.all, page: params[:page], items: NUMBER_PAGE_20)
+    end
+
+    def show
+      @pagy, @posts = pagy(@user.posts, page: params[:page], items: NUMBER_PAGE_10)
+    end
+
+    private
+
+    def set_user
+      @user = User.find_by(id: params[:id])
     end
   end
 end
