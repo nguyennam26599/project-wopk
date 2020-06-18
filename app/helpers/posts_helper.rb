f5243768a1aabbd5b4bb7ef43c6c13d243ee72ca
Huy Hung <huyo.hung98@gmail.com>
2020-06-18 09:56:03 +0700

    refactor: add link style

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,4 +4,17 @@ module PostsHelper
   def tagsinput_data(post)
     post.tags.pluck(:name).join(',')
   end
+
+  def post_status(post)
+    case post.status
+    when Post::PENDING_STATUS
+      'text-success'
+    when Post::DRAFT_STATUS
+      'text-warning'
+    when Post::CLOSE_STATUS
+      'text-danger'
+    else
+      'text-info'
+    end
+  end
 end
