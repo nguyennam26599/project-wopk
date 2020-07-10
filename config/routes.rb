fbe7216e965e41e9f3e302c6a7b823b1f9b9188b
ViHoang <vihoang@MacBook-Pro.local>
2020-07-10 10:47:37 +0700

    fix: conflix

--- config/routes.rb
+++ config/routes.rb
@@ -25,35 +25,16 @@ Rails.application.routes.draw do
         get :search
       end
     end
-  end
-  # end
-
-  devise_scope :user do
-    get 'login', to: 'users/sessions#new'
-    get 'signup', to: 'users/registrations#new'
-    get 'user/password', to: 'users/registrations#edit', as: 'password'
-    get 'user/password', to: 'users/registrations#edit'
-    get 'users/public', to: 'users#show_post_public'
-    get 'users/draft', to: 'users#show_post_draft'
-    get 'users/pending', to: 'users#show_post_pending'
-    get 'users/close', to: 'users#show_post_close'
-  end
-  get 'user/profile', to: 'users#edit', as: 'profile'
-  resources :users, only: %w[update show]
-  namespace :admins do
-    root 'pages#index'
-    # CRUD tags admin
-    resources :tags
-    resources :users
-    # end
-    # Post admin
-    resources :posts
     # end
 
     devise_scope :user do
       get 'login', to: 'users/sessions#new'
       get 'signup', to: 'users/registrations#new'
       get 'user/password', to: 'users/registrations#edit', as: 'password'
+      get 'users/public', to: 'users#show_post_public'
+      get 'users/draft', to: 'users#show_post_draft'
+      get 'users/pending', to: 'users#show_post_pending'
+      get 'users/close', to: 'users#show_post_close'
     end
     get 'user/profile', to: 'users#edit', as: 'profile'
     resources :users, only: %w[update show]
