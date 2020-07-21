1314c21599c179c5d55e4868925ae7258980d9a5
namnx <namnx@gmail.com>
2020-07-21 20:51:36 +0700

    fix: tag list in user detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    @tags = @user.followings.where("following_type = 'Tag'")
     @pagy, @posts = pagy(@user.posts.status_public, page: params[:page])
   end
 
